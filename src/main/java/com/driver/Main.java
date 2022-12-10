c5b5113f17e95b1e449b831ad6a5263f07fae9c6
bharavsai15 <bhargavcme123@gmail.com>
2022-12-10 18:21:04 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+  public  static class A{
+    static void meth(){
+        System.out.println("This is parent class");
+    }
+  }
+
+  public static class B extends A{
+      static  void  meth(){
+          System.out.println("class B extends class A method");
+      }
+  }
+
+    public static void main(String[] args) {
+      B obj = new B();
+      obj.meth();
+    }
 }
